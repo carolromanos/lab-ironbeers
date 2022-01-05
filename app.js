03f99bb1ae2c7bb1d2968b2ba86de909769d6af5
carolromanos <carolina.romanos.roca@gmail.com>
2022-01-05 11:09:47 +0100

    Added solution

--- app.js
+++ app.js
@@ -1,25 +1,95 @@
+// Require packages
 const express = require('express');
-
 const hbs = require('hbs');
+const app = express();
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
-
-const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+// To use partials, we have to register them
+hbs.registerPartials(path.join(__dirname + '/views/partials'));
+
+//Setup HBS and views folder
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname + '/views'));
 
-app.use(express.static(path.join(__dirname, 'public')));
+//To use files from public folder
+app.use(express.static('public'));
 
-// Register the location for handlebars partials here:
-
-// ...
-
-// Add the route handlers here:
-
-app.get('/', (req, res) => {
-  res.render('index');
+//Routes
+app.all('/', (req, res) => {
+	res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+//Route for getting all the beers
+app.all('/beers', (req, res) => {
+	punkAPI
+		.getBeers()
+		.then((responseFromDB) => {
+			console.log('Response is:', responseFromDB);
+
+			res.render('allBeers', { beers: responseFromDB });
+		})
+		.catch((error) => console.log(error));
+});
+
+// With async/await
+/*
+app.get('/beers', async (req, res) => {
+  try {
+    const apiResponse = await punkAPI.getBeers();
+    res.render("beers", { beers: apiResponse } );
+  } catch (err) {
+    console.log("Error ", err);
+  }
+});
+*/
+
+//Route for getting a random beer
+app.all('/random-beer', (req, res) => {
+	punkAPI
+		.getRandom()
+		.then((responseFromDB) => {
+			console.log('Response is:', responseFromDB);
+
+			const randomBeer = responseFromDB[0];
+			console.log('the beer:', randomBeer);
+
+			res.render('random-beer', randomBeer);
+		})
+		.catch((error) => console.log(error));
+});
+
+// With async/await
+/*
+app.get("/random-beer", async (req, res) => {
+  try {
+    const apiResponse = await punkAPI.getRandom();
+    // const oneRandomBeer = apiResponse[0]
+    console.log(apiResponse[0])
+    res.render("randomBeer", { randomBeer: apiResponse } );
+  } catch(err) {
+    console.log("Error ", err)
+  }
+})
+*/
+
+
+// Bonus 6
+app.all('/:id', (req, res) => {
+	const id = req.params.id;
+
+	punkAPI
+		.getBeer(id)
+		.then((responseFromDB) => {
+			console.log('Response is:', responseFromDB);
+
+			const singleBeer = responseFromDB[0];
+			console.log('the beer:', singleBeer);
+
+			res.render('single-beer', singleBeer);
+		})
+		.catch((err) => console.log(err));
+});
+
+app.listen(3000, () => console.log('App is running on port 3000 🎯'));
